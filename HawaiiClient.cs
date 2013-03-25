125ef18446b447f94e92438a3f7a7c53206ebb35
Eirik Daleng Haukedal <edh@steria.no>
2013-03-25 21:31:17 +0000

    Initial commit

---
+++ HawaiiClient.cs
@@ -0,0 +1,24 @@
+﻿// -
+// <copyright file="HawaiiClient.cs" company="Microsoft Corporation">
+//    Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -
+
+namespace OcrSampleAppLite
+{
+    using System.Windows;
+
+    /// <summary>
+    /// HawaiiClient stores the Hawaii client identity.
+    /// </summary>
+    /// <remarks>
+    /// Please consult the "Hawaii Installation Guide" on how to obtain credentials
+    /// </remarks>
+    public static class HawaiiClient
+    {
+        /// <summary>
+        /// The Hawaii Application Id.
+        /// </summary>
+        public const string HawaiiApplicationId = "8348f696-3a91-4cf5-a7f9-fb0c9fad24ba";
+    }
+}
