8079df5643dcff7f414670cfe55913e7df7b2839
Eirik Daleng Haukedal <edh@steria.no>
2013-03-26 11:10:50 +0000

    Added splitter page for recipe ocr

---
+++ SplitterPivotPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace ReciCam.Windows.Phone
+{
+    public partial class SplitterPivotPage : PhoneApplicationPage
+    {
+        public SplitterPivotPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SplitterPivotPage_OnLoad(object sender, RoutedEventArgs e)
+        {
+            ListBoxPhotos.ItemsSource = ((App) Application.Current).RecipeService.RecipePhotos;
+        }
+    }
+}
