a6f213ef27523b7ad77a807758ba023eea0f8066
Eirik Daleng Haukedal <edh@steria.no>
2013-03-26 08:39:11 +0000

    Added camera

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,19 +7,31 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
 
 namespace OcrSampleAppLite
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private CameraCaptureTask ctask;
+
         public MainPage()
         {
             InitializeComponent();
+
+            ctask = new CameraCaptureTask();
+            
+            ctask.Completed += CtaskOnCompleted;
+        }
+
+        private void CtaskOnCompleted(object sender, PhotoResult photoResult)
+        {
+            throw new NotImplementedException();
         }
 
         private void New_Click(object sender, RoutedEventArgs e)
         {
-
+            ctask.Show();
         }
     }
 }
