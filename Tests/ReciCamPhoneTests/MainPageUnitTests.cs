c4ebac005ac440b2565b5b8ee25120b839464833
Eirik Daleng Haukedal <edh@steria.no>
2013-03-29 11:15:41 +0000

    Added unit tests for app

---
+++ Tests/ReciCamPhoneTests/MainPageUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace ReciCamPhoneTests
+{
+    [TestClass]
+    public class MainPageUnitTests
+    {
+        [TestMethod]
+        public void TestThatCameraIsWorking()
+        {
+            Assert.AreEqual(true, false);
+        }
+    }
+}
