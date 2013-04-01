15fe1aa7051e75e0e6400ddfeb1bb3cd1cf0b9a7
Eirik Daleng Haukedal <edh@steria.no>
2013-04-01 13:22:49 +0100

    New menu bar for main page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -15,38 +15,14 @@ namespace ReciCam.Windows.Phone
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        private CameraCaptureTask ctask;
-
-        private ApplicationBarIconButton btnNew;
-
         public MainPage()
         {
             InitializeComponent();
-
-            ctask = new CameraCaptureTask();
-            ctask.Completed += CtaskOnCompleted;
-
-            ApplicationBar = new ApplicationBar();
-            ApplicationBar.IsVisible = true;
-            ApplicationBar.IsMenuEnabled = true;
-            btnNew = new ApplicationBarIconButton(new Uri("/Assets/ModernUI/appbar.new.png", UriKind.Relative));
-            btnNew.Text = "New";
-            ApplicationBar.Buttons.Add(btnNew);
-            btnNew.Click += new EventHandler(New_Click);
-            btnNew.IsEnabled = true;
-
-        }
-
-        private void CtaskOnCompleted(object sender, PhotoResult photoResult)
-        {
-            ((App)Application.Current).RecipeService.AddRecipePhoto(RecipePhoto.CreateFrom(photoResult));
-
-            NavigationService.Navigate(new Uri("/PageAddContent.xaml", UriKind.Relative));
         }
 
         private void New_Click(object sender, EventArgs e)
         {
-            ctask.Show();
+            NavigationService.Navigate(new Uri("/PageAddContent.xaml", UriKind.Relative));
         }
     }
 }
